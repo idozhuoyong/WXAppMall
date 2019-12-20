5a2987c303554952a411777cd73acb7d38b34697
小卓子 <idozhuoyong@qq.com>
2019-12-20 17:47:39 +0800

    index

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,66 +1,27 @@
 // pages/index/index.js
 
 Page({
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        inputShowed: false,
+        inputVal: ""
+    },
+    onLoad() {
+        this.setData({
+            search: this.search.bind(this)
+        })
+    },
+    search(value) {
+        console.log("search: " + value);
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve([{ text: '搜索结果', value: 1 }, { text: '搜索结果2', value: 2 }])
+            }, 200)
+        })
+    },
+    selectResult(e) {
+        console.log('select result', e.detail)
+    },
 })
