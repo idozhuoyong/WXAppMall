de7688950075f68401e9e503387ca68c40c19dd7
小卓子 <idozhuoyong@qq.com>
2019-12-23 17:38:02 +0800

    index

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,18 +9,18 @@ Page({
         inputVal: ""
     },
     onLoad() {
-        this.setData({
-            search: this.search.bind(this)
-        })
+        // this.setData({
+        //     search: this.search.bind(this)
+        // })
     },
-    search(value) {
-        console.log("search: " + value);
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve([{ text: '搜索结果', value: 1 }, { text: '搜索结果2', value: 2 }])
-            }, 200)
-        })
-    },
+    // search(value) {
+    //     console.log("search: " + value);
+    //     return new Promise((resolve, reject) => {
+    //         setTimeout(() => {
+    //             resolve([{ text: '搜索结果', value: 1 }, { text: '搜索结果2', value: 2 }])
+    //         }, 200)
+    //     })
+    // },
     selectResult(e) {
         console.log('select result', e.detail)
     },
