ae5bc69765355827ea2afdc5d9ad8b6f9616e69e
小卓子 <idozhuoyong@qq.com>
2019-12-25 17:58:25 +0800

    notice

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,48 @@
 // pages/index/index.js
+const WXAPI = require("apifm-wxapi")
+WXAPI.init('idozhuoyong') // 初始化使用专属域名
 
 Page({
     /**
      * 页面的初始数据
      */
     data: {
-        inputShowed: false,
-        inputVal: ""
+        noticeList: [], // 公告列表
+        goodsCategory: [], // 商品类别
     },
     onLoad() {
-        // this.setData({
-        //     search: this.search.bind(this)
-        // })
+        // 获取公告
+        this.getNoticeList();
+        // 获取商品分类
+        this.getGoodsCategory();
     },
-    // search(value) {
-    //     console.log("search: " + value);
-    //     return new Promise((resolve, reject) => {
-    //         setTimeout(() => {
-    //             resolve([{ text: '搜索结果', value: 1 }, { text: '搜索结果2', value: 2 }])
-    //         }, 200)
-    //     })
-    // },
+
+    // 获取公告
+    getNoticeList() {
+        WXAPI.noticeList({
+            pageSize: "5"
+        }).then(res => {
+            // console.log(res);
+            if (res.code === 0) {
+                this.setData({
+                    noticeList: res.data.dataList
+                });
+            }
+        });
+    },
+    // 获取商品分类
+    getGoodsCategory() {
+        WXAPI.goodsCategory().then(res => {
+            console.log(res);
+            if (res.code === 0) {
+                this.setData({
+                    goodsCategory: res.data
+                });
+            }
+        });
+    },
+
+    // 搜索
     selectResult(e) {
         console.log('select result', e.detail)
     },
