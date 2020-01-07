e5d77cfbc3fe049b5f6928c405f843505bec3381
小卓子 <idozhuoyong@qq.com>
2020-01-07 17:08:10 +0800

    分类

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,66 +1,36 @@
 // pages/category/category.js
+const WXAPI = require("apifm-wxapi")
+WXAPI.init('idozhuoyong') // 初始化使用专属域名
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        navList: [{
+            name: "上装",
+            id: "1"
+        }, {
+            name: "裤装",
+            id: "2"
+        }, {
+            name: "特价区",
+            id: "3"
+        }, {
+            name: "裙装",
+            id: "4"
+        }],
+        navSelectedIndex: "2",
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        WXAPI.goodsCategory().then(res => {
+            console.log(res);
+        });
+    }
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
